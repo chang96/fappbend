fa7565226fe18af8175d376e86a05c054996411c
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-09-21 13:40:17 +0100

    all coins

--- taapi/macd/ema26.js
+++ taapi/macd/ema26.js
@@ -3,7 +3,7 @@ const ema26 = require('../ema/ema26')
 
 module.exports.t26 = async(coindata26) => {
     try {
-        let twentysix = await ema12.see(12, 12, coindata26)
+        let twentysix = await ema26.see(12, 12, coindata26)
         return twentysix
     } catch (err) {
         console.log(err)
