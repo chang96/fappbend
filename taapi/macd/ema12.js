5cbdc6b4ebe39f17172a344b903b6c6765682f54
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-09-21 15:53:23 +0100

    all coins

--- taapi/macd/ema12.js
+++ taapi/macd/ema12.js
@@ -3,6 +3,7 @@ const ema12 = require('../ema/ema')
 module.exports.t12 = async(coindata12) => {
     try {
         let a = await coindata12
+        a.splice(488, 12)
         console.log(a.length)
         let twelve = await ema12.see(12, 12, a)
         return twelve
