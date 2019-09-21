feb6b4b921cf36182919bf69c7ad3f0c5f50545d
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-09-21 13:28:50 +0100

    all coins

--- taapi/index.js
+++ taapi/index.js
@@ -13,13 +13,14 @@ const time = '15m'
 
 module.exports.histogram = async(close, close1) => {
     try {
-        // let c = await close;
-        // let e = await close1
-        let c = await e12.t12(close)
-        let e = await e26.t26(close1)
         console.log('c:', c.length, 'e:', e.length)
-            //let [c, e] = await Promise.all([close, close1])
-            //let d = await (ema.see(12, 12, c))
+        let c1 = await close;
+        let e1 = await close1
+        let c = await e12.t12(c1)
+        let e = await e26.t26(e1)
+
+        //let [c, e] = await Promise.all([close, close1])
+        //let d = await (ema.see(12, 12, c))
         macd = await diff.diff(c, e)
         let histogram = await hist.signal(macd)
             //return hist
