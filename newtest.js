5460a1bb9053aa9bf92db67c308d425c7d725002
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-09-22 19:53:52 +0100

    all coins

--- newtest.js
+++ newtest.js
@@ -114,7 +114,8 @@ let found = async(size, volume) => {
                         //console.log(a[2].length)
                         //b[b.length - 1] < 35 || b[b.length - 2] < 35 || b[b.length - 3] < 35 || b[b.length - 4] < 35 || b[b.length - 5] < 35 || b[b.length - 6] < 35 || b[b.length - 7] < 35)
                         // console.log(b[b.length - 1] < 35, b[b.length - 2] < 35, b[b.length - 3] < 35, b[b.length - 4] < 35, b[b.length - 5] < 35, b[b.length - 6] < 35, b[b.length - 7] < 35)
-                    if (testing(mymyhist) && (b[b.length - 1] < 35 || b[b.length - 2] < 35 || b[b.length - 3] < 35 || b[b.length - 4] < 35 || b[b.length - 5] < 35 || b[b.length - 6] < 35 || b[b.length - 7] < 35)) {
+                    let z = 25
+                    if (testing(mymyhist) && (b[b.length - 1] < z || b[b.length - 2] < z || b[b.length - 3] < z || b[b.length - 4] < z || b[b.length - 5] < z || b[b.length - 6] < z || b[b.length - 7] < z)) {
                         //console.log({ name: candle.name, ma: a[0][6], si: a[1][6], hi: a[2][6] })
                         console.log({ name: candle.name, hi: mymyhist[mymyhist.length - 1] })
                             //arr.push({ name: candle.name, ma: a[0][6], si: a[1][6], hi: a[2][6] })
