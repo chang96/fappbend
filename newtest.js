9bcadd7dd226b62d8d79f19549505eee18a58b85
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-09-25 21:09:04 +0100

    all coins

--- newtest.js
+++ newtest.js
@@ -104,7 +104,7 @@ let found = async(size, volume) => {
                     let z = 35
                     if (testing(mymyhist) && (b[b.length - 1] < z || b[b.length - 2] < z || b[b.length - 3] < z || b[b.length - 4] < z || b[b.length - 5] < z || b[b.length - 6] < z || b[b.length - 7] < z)) {
                         //console.log({ name: candle.name, ma: a[0][6], si: a[1][6], hi: a[2][6] })
-                        console.log({ name: candle.name, hi: mymyhist[mymyhist.length - 1] })
+                        console.log({ name: candle.name, hi: mymyhist[mymyhist.length - 1], rsi: b[b.length - 1] })
                             //arr.push({ name: candle.name, ma: a[0][6], si: a[1][6], hi: a[2][6] })
                             //arr.push({ name: candle.name })
                         return candle.name
