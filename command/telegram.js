c3ce6ae50924d1fba8707f18e68c61d9d9989d2d
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-10-10 14:16:19 +0100

    all coins

--- command/telegram.js
+++ command/telegram.js
@@ -43,8 +43,8 @@ module.exports.tel = function(bot, Coin, User, api) {
                         //let auth = ['Cha_ng']
                         //if (find(User, data) || auth.indexOf(`${data.from.username}`) !== -1) {
                         //if (anyone) {
+                    console.log(find(User, data))
                     if (find(User, data) == true) {
-                        console.log('gfhjkhgf')
                         let u = `time${t}`
                             //console.log(u)
                         bot.sendMessage(msg.chat.id, `lastupdated: ${a[u]}\ncoin:${a[t]}`)
