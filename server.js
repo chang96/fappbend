a53b7ca146f619766154259015067155a2f324a1
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-11-09 20:28:04 +0100

    all done

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ const findSendme = async function(coins) {
     }))
 
 }
-let arr2
+let arr2 = []
 const sendMe = async function() {
     await Coin.findOne({ mymyid: 'string' }, async function(err, coin) {
         let arr = ['t15m', 't30m', 't1h', 't4h', 't1d', 't1w']
