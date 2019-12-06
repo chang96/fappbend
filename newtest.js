680e56c7bd406119beba7cf0d7d298c71f76b740
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-12-06 13:10:51 +0100

    multiple candles

--- newtest.js
+++ newtest.js
@@ -375,13 +375,13 @@ let found1 = async(size, volume, rs) => {
                     // }
                     if (crossover(mymyhist) && candle.pip500[candle.pip500.length - 1] > em.ema && candle.pip500[candle.pip500.length - 2] < em.ema && voltesting(vtday, smav)) {
                         console.log(alpha)
-                        return `${candle.name}~   `
+                        return `${candle.name}`
                     } else if (candle.pip500[candle.pip500.length - 2] < em.ema && candle.pip500[candle.pip500.length - 1] > em.ema && voltesting(vtday, smav) && histinc1(mymyhist)) {
                         console.log(alpha)
-                        return `${candle.name}    `
+                        return `${candle.name}`
                     } else if (candle.pip500[candle.pip500.length - 1] > em.ema && crossover(mymyhist) && voltesting(vtday, smav)) {
                         console.log(alpha)
-                        return `${candle.name}:~  `
+                        return `${candle.name}...`
                     }
                     // else if (histinc1(mymyhist) && voltesting0(vtday, smav) && candle.pip500[candle.pip500.length - 1] > em.ema && candle.pip500[candle.pip500.length - 23] < em.ema && candle.pip500[candle.pip500.length - 2] < em.ema) {
                     //     return candle.name
