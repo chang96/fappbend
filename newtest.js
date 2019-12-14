75f0ed91a7dc5e5c934c7d8db7becd66c4bed7d1
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-12-14 21:14:25 +0100

    l

--- newtest.js
+++ newtest.js
@@ -243,7 +243,7 @@ let found = async(size, volume, rs) => {
                     let alpha = alph.toFixed(1)
                     let mymyhist1 = await mymacd.histogram(candle.pip100, candle.pip200)
                         //console.log(mymyhist1)
-                    let em = ema.see(55, 55, candle.pip500)
+                    let em = await ema.see(55, 55, candle.pip500)
                     let mymyhist = mymyhist1.histogram
                         //let mymysig = mymyhist1.signal
                     let mymymac = mymyhist1.macd
