b98bdb7ece0994bbaad3785da3817680ff0da41a
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-12-16 17:06:13 +0100

    multi candles

--- server.js
+++ server.js
@@ -292,7 +292,7 @@ async function highest(array, n) {
         let w = await Number(b.split('').slice(0, 3).join(''))
         let x = await Number(a.split('').slice(0, 3).join(''))
         console.log(w)
-        return w - x
+        return x - w
     }).slice(0, n)
     return highest
 }
