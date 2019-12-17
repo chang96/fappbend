ba4f0aa310286a5dd13f3d520d88023b0195e469
Rahman Adebayo Orewole <rorewole@gmail.com>
2019-12-17 14:32:17 +0100

    multi candles

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ let arr2 = []
 let tradds2 = []
 const sendMe = async function() {
     await Coin.findOne({ mymyid: 'string' }, async function(err, coin) {
-        let arr = ['t1m', 't3m', 't5m', 't15m', 't30m', 't1h', 't4h', 't1d', 't1w']
+        let arr = ['t3m', 't5m', 't15m', 't30m', 't1h', 't4h', 't1d', 't1w']
         if (err) return err
         if (coin) {
             //console.log(findSendme(coin))
@@ -294,7 +294,7 @@ async function ccc() {
 
     })
     let b = await myarr.concat(a.t4h, a.t1d, a.t1w)
-    let c = await b.forEach(async(e) => yourarr.indexOf(e) < 0 && !e.match(rgx4) && !e.match(rgx5) ? await yourarr.push(e) : console.log('e de'))
+    let c = await b.forEach(async(e) => yourarr.indexOf(e) < 0 && !e.match(rgx4) && !e.match(rgx5) ? await yourarr.push(e) : null)
         // console.log(yourarr)
     return yourarr
 }
