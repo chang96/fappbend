e2ccdd6ee07928f7a2e628cb9738baaf9d307065
Rahman Adebayo Orewole <rorewole@gmail.com>
2020-01-23 15:00:13 +0100

    stock added

--- newtest.js
+++ newtest.js
@@ -422,7 +422,7 @@ let found1 = async(size, volume, rs) => {
                         return `${candle.name }`
                     }
 
-                    // else if (histinc1(mymyhist) && voltesting0(vtday, smav) && candle.pip500[candle.pip500.length - 1] > em.ema && candle.pip500[candle.pip500.length - 23] < em.ema && candle.pip500[candle.pip500.length - 2] < em.ema) {
+                    //// else if (histinc1(mymyhist) && voltesting0(vtday, smav) && candle.pip500[candle.pip500.length - 1] > em.ema && candle.pip500[candle.pip500.length - 23] < em.ema && candle.pip500[candle.pip500.length - 2] < em.ema) {
                     //     return candle.name
                     // }
                     // else if ((voltesting0(vtday, smav) && tickingfromnegative(mymyhist)) && rrssii) {
