24371673986a30f46977b433d51bd1f15d8a83ac
chang <rrahmanade@gmail.com>
2020-03-15 12:50:39 +0100

    rr

--- server.js
+++ server.js
@@ -387,6 +387,7 @@ async function ccc2(tf) {
         let name = await n.name
         return name
     }))
+    console.log(nameArr)
     let c = await nameArr.forEach(async(e) => yourarr.indexOf(e) < 0 && !e.match(rgx4) && !e.match(rgx5) ? await yourarr.push(e) : null)
         // console.log(yourarr)
     return yourarr
@@ -560,7 +561,7 @@ let search1w = function(size, volume, rs) {
         //api.sendMessage({ chat_id: 954135852, text: 'saved 4h' })
     setTimeout(search4h, 1000 * 60 * 1.8, '4h', 100000, rsii)
 }
-setTimeout(search1w, 1000 * 60 * 0.25, '1w', 100000, rsii)
+setTimeout(search15m, 1000 * 60 * 0.25, '15m', 100000, rsii)
 app.get('/', function(req, res) {
         res.send(`
     1) Access candles 15m, 1h, 4h. Update is every 6mins.
