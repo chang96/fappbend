1e13604ff73af831ab1d09137f44359789c5ec2d
chang <rrahmanade@gmail.com>
2020-04-05 15:57:09 +0100

    up

--- newtest.js
+++ newtest.js
@@ -370,41 +370,47 @@ let rgx1 = /BTC$/
 let rgx2 = /ETH$/
 let rgx3 = /USDT$/
 let found = async(size, volume, rs, eyoar) => {
+    try{
     let arr = []
     candles = await find2(size, volume, eyoar)
-    return Promise.all(
-            candles.map(async function(candle) {
-                try {
-                    let smav = await v(candle.v, 20)
-                    let vtday = candle.v3
-                    let alph = vtday[vtday.length - 1] / smav[smav.length - 1]
-                    let alpha = alph == NaN ? 0 : alph.toFixed(1)
-                    let mymyhist1 = await mymacd.histogram(candle.pip100, candle.pip200)
-                    let em = await ema.see(55, 55, candle.pip500)
-                    let mymyhist = mymyhist1.histogram
-                    let mymymac = mymyhist1.macd
-                   let b = await myrsi.rsi(candle.pip)
-                   let stochs = await stochastic.stochRSI(candle.stochClose)
-                    let K = stochs.k
-                    let D = stochs.d
-                    let renkobars = await renko(candle.forrenko)
+    let r = await callCandles(mymap, candles)
+    return r
+    } catch(e){
+        console.log(e)
+    }
+    // return Promise.all(
+    //         candles.map(async function(candle) {
+    //             try {
+    //                 let smav = await v(candle.v, 20)
+    //                 let vtday = candle.v3
+    //                 let alph = vtday[vtday.length - 1] / smav[smav.length - 1]
+    //                 let alpha = alph == NaN ? 0 : alph.toFixed(1)
+    //                 let mymyhist1 = await mymacd.histogram(candle.pip100, candle.pip200)
+    //                 let em = await ema.see(55, 55, candle.pip500)
+    //                 let mymyhist = mymyhist1.histogram
+    //                 let mymymac = mymyhist1.macd
+    //                let b = await myrsi.rsi(candle.pip)
+    //                let stochs = await stochastic.stochRSI(candle.stochClose)
+    //                 let K = stochs.k
+    //                 let D = stochs.d
+    //                 let renkobars = await renko(candle.forrenko)
                        
                         
                     
-                    if(renkobars[1][0] == '+' &&renkobars[1][1] == '-' ){
-                        return {name:`${candle.name}` }
-                    }
+    //                 if(renkobars[1][0] == '+' &&renkobars[1][1] == '-' ){
+    //                     return {name:`${candle.name}` }
+    //                 }
 
-                } catch (err) {
-                    console.log(err)
-                }
-            })).then((arr) => {
-            return Promise.all(arr.filter(function(a) {
-                if ((a !== undefined) ) {
-                    return a
-                }
-            }))
-        }).catch(err => console.log(err))
+    //             } catch (err) {
+    //                 console.log(err)
+    //             }
+    //         })).then((arr) => {
+    //         return Promise.all(arr.filter(function(a) {
+    //             if ((a !== undefined) ) {
+    //                 return a
+    //             }
+    //         }))
+    //     }).catch(err => console.log(err))
 }
 
 let found1 = async(size, volume, rs) => {
@@ -412,190 +418,57 @@ let found1 = async(size, volume, rs) => {
     let arr = []
     let candles = await find1(size, volume)
     let r = await callCandles(mymap, candles)
-    console.log(r)
+   // console.log(r)
     return r
    }catch(e){
        console.log(e)
     }
-    // return Promise.all(
-    //         candles.map(async function(candle) {
-               
-    //             try {
-    //                 // let smav = await v(candle.v, 20)
-    //                 // let vtday = candle.v3
-    //                 // let alph = vtday[vtday.length - 1] / smav[smav.length - 1]
-    //                 // let alpha = alph == NaN ? 0 : alph.toFixed(1)
-    //                 let currentPrice = candle.pip[candle.pip.length -1]
-    //                 let secondToTheLastPrice = candle.pip[candle.pip.length - 2]
-    //                 let acurrentPrice = candle.apip[candle.apip.length -1]
-    //                 let asecondToTheLastPrice = candle.apip[candle.apip.length -2]
-    //                 let mymyhist1 = await mymacd.histogram(candle.pip100, candle.pip200)
-    //                 let em55 = await ema.see(55, 55, candle.pip500)
-    //                 let em99  = await ema.see(99, 99, candle.pip500)
-    //                 let em200 = await ema.see(200, 200, candle.pip500)
-    //                 let mymyhist = mymyhist1.histogram
-    //                 let mymymac = mymyhist1.macd
-    //                 let b = await myrsi.rsi(candle.pip)
-    //                 let stochs = await stochastic.stochRSI(candle.stochClose)
-    //                 let K = await stochs.k
-    //                 let D = await stochs.d
-    //                 let renkobars = await renko(candle.forrenko)
-    //                 let amymyhist1 = await mymacd.histogram(candle.apip100, candle.apip200)
-    //                 let aem55 = await ema.see(55, 55, candle.apip500)
-    //                 let aem99  = await ema.see(99, 99, candle.apip500)
-    //                 let aem200 = await ema.see(200, 200, candle.apip500)
-    //                 let amymyhist = mymyhist1.histogram
-    //                 let amymymac = mymyhist1.macd
-    //                 let ab = await myrsi.rsi(candle.apip)
-    //                 let astochs = await stochastic.stochRSI(candle.astochClose)
-    //                 let aK = await astochs.k
-    //                 let aD = await astochs.d
-                       
-                      
-
-
-    //                 if(renkobars[1][0] == '+'){
-    //                     return {name:`${candle.name}`, desc:'r++'} 
-    //                 } 
-    //                 if(renkobars[1][0] == '+' && renkobars[1][1] == '-'){
-    //                     return {name:`${candle.name}`, desc:'-r+'} 
-    //                 }
-    //                 if(renkobars[1][0] == '-'){
-    //                     return {name:`${candle.name}`, desc:'r-'} 
-    //                 } 
-    //                 if(renkobars[1][0] == '-' && renkobars[1][1] == '+'){
-    //                     return {name:`${candle.name}`, desc:'+r-'} 
-    //                 }
-    //                 if(currentPrice >= em55 && secondToTheLastPrice < em55){
-    //                     return {name:`${candle.name}`, desc:'+em55'} 
-    //                 }
-    //                 if(currentPrice >= em99 && secondToTheLastPrice < em99 ){
-    //                     return {name:`${candle.name}`, desc:'+em99'} 
-    //                 }
-    //                 if(currentPrice >= em200 && secondToTheLastPrice < em200){
-    //                     return {name:`${candle.name}`, desc:'+em200'} 
-    //                 }
-    //                 if(currentPrice < em55 && secondToTheLastPrice >= em55 ){
-    //                     return {name:`${candle.name}`, desc:'-em55'} 
-    //                 }
-    //                 if(currentPrice < em99 && secondToTheLastPrice >= em99 ){
-    //                     return {name:`${candle.name}`, desc:'-em99'} 
-    //                 }
-    //                 if(currentPrice < em200 && secondToTheLastPrice >= em200 ){
-    //                     return {name:`${candle.name}`, desc:'-em200'} 
-    //                 }
-    //                 if(K[0] >= D[0] && K[1] < D[1]){
-    //                     return {name:`${candle.name}`, desc:'+str'} 
-    //                 }
-    //                 if(K[0] >= D[0] && K[1] < D[1] && K[0] >= 50){
-    //                     return {name:`${candle.name}`, desc:'+str50'} 
-    //                 }
-    //                 if(K[0] < D[0] && K[1] >= D[1]){
-    //                     return {name:`${candle.name}`, desc:'-str'} 
-    //                 }
-    //                 if(crossover(mymyhist)){
-    //                     return {name: `${candle.name}`, desc:'hist+'}
-    //                 }
-    //                 if(crossunder(mymyhist)){
-    //                     return {name: `${candle.name}`, desc: 'hist-'}
-    //                 }
-    //                 if(b <= 30 ){
-    //                     return {name: `${candle.name}`, desc: '<rsi'}
-    //                 } 
-    //                 if(b >= 70 ){
-    //                     return {name: `${candle.name}`, desc: '>rsi'}
-    //                 } 
-
-                    
-    //                 if(acurrentPrice >= aem55 && asecondToTheLastPrice < aem55){
-    //                     return {name:`${candle.name}`, desc:'+aem55'} 
-    //                 }
-    //                 if(acurrentPrice >= aem99 && asecondToTheLastPrice < aem99 ){
-    //                     return {name:`${candle.name}`, desc:'+em99'} 
-    //                 }
-    //                 if(acurrentPrice >= aem200 && asecondToTheLastPrice < aem200){
-    //                     return {name:`${candle.name}`, desc:'+aem200'} 
-    //                 }
-    //                 if(acurrentPrice < aem55 && asecondToTheLastPrice >= aem55 ){
-    //                     return {name:`${candle.name}`, desc:'-aem55'} 
-    //                 }
-    //                 if(acurrentPrice < em99 && asecondToTheLastPrice >= aem99 ){
-    //                     return {name:`${candle.name}`, desc:'-aem99'} 
-    //                 }
-    //                 if(acurrentPrice < aem200 && asecondToTheLastPrice >= aem200 ){
-    //                     return {name:`${candle.name}`, desc:'-aem200'} 
-    //                 }
-    //                 if(aK[0] >= aD[0] && aK[1] < aD[1]){
-    //                     return {name:`${candle.name}`, desc:'+astr'} 
-    //                 }
-    //                 if(aK[0] >= aD[0] && aK[1] < aD[1] && aK[0] >= 50){
-    //                     return {name:`${candle.name}`, desc:'+astr50'} 
-    //                 }
-    //                 if(aK[0] < aD[0] && aK[1] >= aD[1]){
-    //                     return {name:`${candle.name}`, desc:'-astr'} 
-    //                 }
-    //                 if(crossover(amymyhist)){
-    //                     return {name: `${candle.name}`, desc:'ahist+'}
-    //                 }
-    //                 if(crossunder(amymyhist)){
-    //                     return {name: `${candle.name}`, desc: 'ahist-'}
-    //                 }
-    //                 if(ab <= 30 ){
-    //                     return {name: `${candle.name}`, desc: '<arsi'}
-    //                 } 
-    //                 if(ab >= 70 ){
-    //                     return {name: `${candle.name}`, desc: '>arsi'}
-    //                 } 
-    //             } catch (err) {
-    //                 console.log(err)
-    //             }
-    //         }))
-            
-        //     .then((arr) => {
-        //     return Promise.all(arr.filter(function(a) {
-        //         if ((a !== undefined)) {
-        //             return a
-        //         }
-        //     }))
-        // }). catch(e) => console.log(e)
+   
 }
 
 let found2 = async(size, volume, rs, eyoar) => {
-    let arr = []
-        //console.log(3)
-    let candles = await find2(size, volume, eyoar)
-    return Promise.all(
-            candles.map(async function(candle) {
-                try {
-                    //let smav = await v(candle.v, 20)
-                    //let vtday = candle.v3
-                    //let alph = vtday[vtday.length - 1] / smav[smav.length - 1]
-                    //let alpha = alph == NaN ? 0 : alph.toFixed(1)
-                    let mymyhist1 = await mymacd.histogram(candle.pip100, candle.pip200)
-                        
-                    let em = await ema.see(55, 55, candle.pip500)
-                    let mymyhist = mymyhist1.histogram
-                    let mymymac = mymyhist1.macd
-                   let b = await myrsi.rsi(candle.pip)
-                    let stochs = await stochastic.stochRSI(candle.stochClose)
-                    let K = stochs.k
-                    let D = stochs.d
-                    let renkobars = await renko(candle.forrenko)
-                       
-                    if(renkobars[1][0] == '+'){
-                        return {name:`${candle.name}`}
-                    }
+    try{
+        let arr = []
+        let candles = await find2(size, volume, eyoar)
+        let r = await callCandles(mymap, candles)
+        return r
+    } catch(e){
+        console.log(e)
+    }
+    
 
-                } catch (err) {
-                    console.log(err)
-                }
-            })).then((arr) => {
-            return Promise.all(arr.filter(function(a) {
-                if ((a !== undefined) )  {
-                    return a
-                }
-            }))
-        }).catch(err => console.log(err))
+    // return Promise.all(
+    //         candles.map(async function(candle) {
+    //             try {
+    //                 //let smav = await v(candle.v, 20)
+    //                 //let vtday = candle.v3
+    //                 //let alph = vtday[vtday.length - 1] / smav[smav.length - 1]
+    //                 //let alpha = alph == NaN ? 0 : alph.toFixed(1)
+    //                 let mymyhist1 = await mymacd.histogram(candle.pip100, candle.pip200)
+                        
+    //                 let em = await ema.see(55, 55, candle.pip500)
+    //                 let mymyhist = mymyhist1.histogram
+    //                 let mymymac = mymyhist1.macd
+    //                let b = await myrsi.rsi(candle.pip)
+    //                 let stochs = await stochastic.stochRSI(candle.stochClose)
+    //                 let K = stochs.k
+    //                 let D = stochs.d
+    //                 let renkobars = await renko(candle.forrenko)
+                       
+    //                 if(renkobars[1][0] == '+'){
+    //                     return {name:`${candle.name}`}
+    //                 }
+
+    //             } catch (err) {
+    //                 console.log(err)
+    //             }
+    //         })).then((arr) => {
+    //         return Promise.all(arr.filter(function(a) {
+    //             if ((a !== undefined) )  {
+    //                 return a
+    //             }
+    //         }))
+    //     }).catch(err => console.log(err))
 }
 
 module.exports.founnd = async(size, volume, rs, eyoar) => {
