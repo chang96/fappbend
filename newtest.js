cd05f5f1dde24ef63d9e404071346354f0480ead
chang <rrahmanade@gmail.com>
2022-03-07 15:43:53 +0100

    done

--- newtest.js
+++ newtest.js
@@ -117,10 +117,18 @@ let find1 = async(size, volume) => {
             const volumeChange = (Number(volumepush[volumepush.length - 1]) - Number(volumepush[volumepush.length - 2])) / Number(volumepush[volumepush.length - 2])
             let em200 = await ema.see(200, 200, close100)
             let b = await myrsi.rsi(close200, 20)
+            let em20 = await ema.see(20, 20, [...close200])
+            let em100 = await ema.see(100, 100, [...close200])
+            let hist = await mymacd.histogram([...close200], [...close200])
+            let histogram = hist.histogram
+            
+            let histpos = histogram.reverse()[0] > 0? true : false
+            let histVal = histogram[0]
             return { name: eyo.symbol, pip100: close100, pip200: close200, pip: close400, v: volumepush, v3: v3, pip500: close500, stochClose: stochClose, forrenko:bars,
                 apip100: aclose100, apip200: aclose200, apip: aclose400, apip500: aclose500, astochClose: astochClose,qv: qv, 
                 volumeChange: Math.ceil(volumeChange*100), volumeChange1h: Math.ceil(volumeChange1h*100), volumeChange7d: Math.ceil(volumeChange7d*100), volumeChange30d: Math.ceil(volumeChange30d*100), volumeChange90d: Math.ceil(volumeChange90d*100), // pip100: close100,  v: volumepush,
-                total_volume: Math.ceil(Number(v3[v3.length -1]) ), ema: em200.ema, rsi: b.reverse()[0], p:stochClose.reverse()[0]
+                total_volume: Math.ceil(Number(v3[v3.length -1]) ), ema: em200.ema, rsi: b.reverse()[0], p:stochClose.reverse()[0],ema20:em20.ema,
+                em100:em100.ema, histogram: histpos, histogramVal:histVal
             }
         })).catch(err => console.log(err))
 
@@ -210,18 +218,11 @@ let find4 = async(size, volume) => {
             
             let v3 = volumepush.slice(19, 26)
             let em200 = await ema.see(200, 200, close100)
-            let em20 = await ema.see(20, 20, [...close200])
-            let em100 = await ema.see(100, 100, [...close200])
-            let hist = await mymacd.histogram([...close200], [...close200])
-            let histogram = hist.histogram
             
-            let histpos = histogram.reverse()[0] > 0? true : false
-            let histVal = histogram[0]
             let b = await myrsi.rsi(close200, 20)
 
             return { name: eyo.symbol, v3: v3, volumeChange: Math.ceil(volumeChange*100), volumeChange1h: Math.ceil(volumeChange1h*100), volumeChange7d: Math.ceil(volumeChange7d*100), volumeChange30d: Math.ceil(volumeChange30d*100), volumeChange90d: Math.ceil(volumeChange90d*100), // pip100: close100,  v: volumepush,
-                total_volume: Math.ceil(Number(v3[v3.length -1]) ), ema: em200.ema, rsi: b.reverse()[0], p:stochClose.reverse()[0],ema20:em20.ema,
-                em100:em100.ema, histogram: histpos, histogramVal:histVal
+                total_volume: Math.ceil(Number(v3[v3.length -1]) ), ema: em200.ema, rsi: b.reverse()[0], p:stochClose.reverse()[0],
             }
         })).catch(err => console.log(err))
 
